c783d6108956ff6617474f4781691b753ef082ae
Felix <gatoab2@gmail.com>
2026-09-06 21:41:36 -0500

    feat: HashLocationStrategy

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,36 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter, withInMemoryScrolling } from '@angular/router';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { importProvidersFrom } from '@angular/core';
-import { MatNativeDateModule } from '@angular/material/core';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { AppComponent } from './app.component';
-import { routes } from './app.routes';
-import { authInterceptor } from './core/interceptors/auth.interceptor';
-import { errorInterceptor } from './core/interceptors/error.interceptor';
-import { loadingInterceptor } from './core/interceptors/loading.interceptor';
+import { bootstrapApplication } from "@angular/platform-browser";
+import { provideRouter, withInMemoryScrolling } from "@angular/router";
+import { provideHttpClient, withInterceptors } from "@angular/common/http";
+import { provideAnimations } from "@angular/platform-browser/animations";
+import { importProvidersFrom } from "@angular/core";
+import { MatNativeDateModule } from "@angular/material/core";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { AppComponent } from "./app.component";
+import { routes } from "./app.routes";
+import { authInterceptor } from "./core/interceptors/auth.interceptor";
+import { errorInterceptor } from "./core/interceptors/error.interceptor";
+import { loadingInterceptor } from "./core/interceptors/loading.interceptor";
+import { HashLocationStrategy, LocationStrategy } from "@angular/common";
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(routes, withInMemoryScrolling({ scrollPositionRestoration: 'top' })),
-    provideHttpClient(withInterceptors([authInterceptor, errorInterceptor, loadingInterceptor])),
+    provideRouter(
+      routes,
+      withInMemoryScrolling({ scrollPositionRestoration: "top" }),
+    ),
+    {
+      provide: LocationStrategy,
+      useClass: HashLocationStrategy,
+    },
+    provideHttpClient(
+      withInterceptors([authInterceptor, errorInterceptor, loadingInterceptor]),
+    ),
     provideAnimations(),
-    importProvidersFrom(MatNativeDateModule, MatDialogModule, MatSnackBarModule),
+    importProvidersFrom(
+      MatNativeDateModule,
+      MatDialogModule,
+      MatSnackBarModule,
+    ),
   ],
 }).catch((err) => console.error(err));
